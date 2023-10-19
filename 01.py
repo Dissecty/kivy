a3a65c8687bbf50c2ece057dc4036e7351be5390
Dissecty <148487674+Dissecty@users.noreply.github.com>
2023-10-19 15:22:38 -0300

    v

--- 01.py
+++ 01.py
@@ -1,1 +1,20 @@
-0
+import kivy
+
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.app import App
+
+import math, random
+
+class GL(GridLayout):
+    def __init__(self):
+        super().__init__()
+
+        self.cols = 3
+
+class MyApp(App):
+    def build(self):
+        return GL()
+    
+MyApp().run()
